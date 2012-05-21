09bd64b34634628afe91554be66115282f1275f1
smjimenez <smjimenez@106-03.info2>
2012-05-21 13:51:16 +0200

    commit metodos

--- smjimenez/src/clase1.java
+++ smjimenez/src/clase1.java
@@ -4,4 +4,8 @@ public class clase1 {
 	public void imprimir (){
 		System.out.println("Soy la clase 1");
 	}
+	
+	public void metodo1_clase1 (){
+		
+	}
 }
--- smjimenez/src/clase2.java
+++ smjimenez/src/clase2.java
@@ -4,5 +4,8 @@ public class clase2 {
 	public void imprimir (){
 		System.out.println("Soy la clase 2");
 	}
-
+	
+	public void metodo2_clase2 (){
+		
+	}
 }
